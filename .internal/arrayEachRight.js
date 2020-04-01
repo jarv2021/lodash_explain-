5bf897a7f4952602e041bf717f96ecdc18ccc0a4
liangqiujie <708985420@qq.com>
2020-04-01 17:55:06 +0800

    save two

--- .internal/arrayEachRight.js
+++ .internal/arrayEachRight.js
@@ -5,6 +5,11 @@
  * @param {Array} [array] The array to iterate over.
  * @param {Function} iteratee The function invoked per iteration.
  * @returns {Array} Returns `array`.
+ * 
+ * 简单来说： == 代表相同， ===代表严格相同, 为啥这么说呢， 
+   这么理解： 当进行双等号比较时候： 先检查两个操作数数据类型，如果相同，
+   则进行===比较， 如果不同， 则愿意为你进行一次类型转换， 转换成相同类型后再进行比较，
+   而===比较时， 如果类型不同，直接就是false.
  */
 function arrayEachRight(array, iteratee) {
   let length = array == null ? 0 : array.length
--- isObjectLike.js
+++ isObjectLike.js
@@ -19,6 +19,26 @@
  *
  * isObjectLike(null)
  * // => false
+ * 
+ * 
+ * 
+ * 您可使用js的typeof来确定js变量的类型：
+
+  typeof运算符返回变量或表达式的类型：
+
+  typeof  0  //返回"number"
+
+  typeof  " "   //返回"string"
+
+  typeof  "Bill"  //返回"string"
+
+  typeof true   //返回"boolean"
+
+  typeof false  //返回"boolean"
+
+  typeof  x   //返回““undefined（假如x没有值）”
+
+  typeof运算符把对象、数组或null返回object。
  */
 function isObjectLike(value) {
   return typeof value === 'object' && value !== null
